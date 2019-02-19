bda5d3a12f933281e4a1e3643d796a4ca630ab1a
coopci <bo.coopci@gmail.com>
2019-02-19 21:50:26 +0800

    加注释

--- src/test/java/gubo/learn/jmockit/FileServiceTest.java
+++ src/test/java/gubo/learn/jmockit/FileServiceTest.java
@@ -8,6 +8,9 @@ import mockit.Tested;
 public class FileServiceTest {
 	@Injectable 
 	FileLoader fileLoader;
+	// jmockit 1.8 可以把 fileLoader 注到fileService.fileLoader。 
+	// 但是 jmockit 1.28  不会把 fileLoader 注到fileService.fileLoader。  会出异常:  java.lang.IllegalStateException: Missing @Injectable for field "gubo.learn.jmockit.FileLoader fileloader" in FileService
+
 	@Tested
 	FileService fileService;// = new FileService();
 
